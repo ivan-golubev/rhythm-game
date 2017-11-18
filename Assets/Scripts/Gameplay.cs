b7699dd43148297dc8adb86e4e0cd01eb1006f53
Ivan Golubev <ivan.golubev@outplay.com>
2017-11-18 18:22:08 +0000

    Fixing the camera, basic obj spawning
    1. Adding a second camera for game objects, configuring depth and other settings
    2. Spawning shperes in a coroutine

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+
+    [SerializeField] private GameObject gameplayRoot;
+    [SerializeField] private GameObject spherePrefab;
+    [SerializeField] private int startDelaySec, spawnDelaySec;
+
+    private bool spawning;
+
+	void Start ()
+	{
+	    spawning = true;
+        StartCoroutine(GameLoop());
+	}
+	
+	void Update () {
+		
+	}
+
+    IEnumerator GameLoop()
+    {
+        yield return new WaitForSeconds(startDelaySec);
+        Vector3 spawnPosition = new Vector3(0.0f, 0.0f, 300.0f);
+        while (spawning)
+        {
+            yield return new WaitForSeconds(spawnDelaySec);
+            var sphere = Instantiate(spherePrefab);
+            sphere.gameObject.SetActive(true);
+            sphere.transform.SetParent(gameplayRoot.gameObject.transform, false);
+            sphere.transform.SetPositionAndRotation(spawnPosition, Quaternion.identity);
+        }
+        yield return null;
+    }
+}
